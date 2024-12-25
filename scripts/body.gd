8cedcb40927909486eb4b91449c29096a9efafc4
Grady Wasil <90996993+Arrangedgodly@users.noreply.github.com>
2024-12-24 20:37:40 -0700

    starting to get the api working

--- scripts/body.gd
+++ scripts/body.gd
@@ -1,4 +1,27 @@
 extends TextEdit
+signal new_last_word(new_word: String)
+
+var check_ready: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_select"):
+		check_ready = true
 
 func _on_text_changed() -> void:
-	pass # Replace with function body.
+	if check_ready:
+		check_ready = false
+		if text.length() > 0:
+			var last_word = get_last_word(text)
+			if last_word != "":
+				new_last_word.emit(last_word)
+
+func get_last_word(input_text: String) -> String:
+	# Split the text into words
+	var words = input_text.strip_edges().split(" ", false)
+	
+	# Return empty string if no words
+	if words.size() == 0:
+		return ""
+	
+	# Return the last word
+	return words[-1]
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,1 +1,10 @@
 extends Control
+
+@onready var body: TextEdit = $Grid/CenterMenu/Body
+@onready var api: Node = $Api
+
+func _ready() -> void:
+	body.new_last_word.connect(_on_body_new_last_word)
+	
+func _on_body_new_last_word(new_word: String) -> void:
+	pass
