ea07e829f06b41e2238d2e4124c16349bcf6bae1
Grady Wasil <90996993+Arrangedgodly@users.noreply.github.com>
2024-12-26 22:03:34 -0700

    finished the api rework to make things less repetitive

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,20 +2,25 @@ extends Control
 
 @onready var body: TextEdit = $Grid/CenterMenu/Body
 @onready var api: Node = $Api
-@onready var rhymes: VBoxContainer = $Grid/LeftMenu/Rhymes
-@onready var adjectives: VBoxContainer = $Grid/LeftMenu/Adjectives
-@onready var sound_alikes: VBoxContainer = $Grid/LeftMenu/SoundAlikes
-@onready var nouns: VBoxContainer = $Grid/LeftMenu/Nouns
-@onready var words: VBoxContainer = $Grid/RightMenu/Words
-@onready var synonyms: VBoxContainer = $Grid/RightMenu/Synonyms
-@onready var antonyms: VBoxContainer = $Grid/RightMenu/Antonyms
-@onready var freq_followers: VBoxContainer = $Grid/RightMenu/FreqFollowers
+
+var menus = {}
+var menu_types = [
+	["LeftMenu", ["Rhymes", "Adjectives", "SoundAlikes", "Nouns"]],
+	["RightMenu", ["Words", "Synonyms", "Antonyms", "FreqFollowers"]]
+]
 
 func _ready() -> void:
+	_setup_menus()
 	body.new_last_word.connect(_on_body_new_last_word)
-	rhymes.word_selected.connect(body.add_last_word)
-	adjectives.word_selected.connect(body.add_last_word)
-	
+
+func _setup_menus() -> void:
+	for menu in menu_types:
+		var menu_path = menu[0]
+		for item in menu[1]:
+			var node = get_node("Grid/" + menu_path + "/" + item)
+			menus[item.to_lower()] = node
+			node.word_selected.connect(body.add_last_word)
+
 func _on_body_new_last_word(new_word: String) -> void:
-	api.rhyme_request(new_word)
-	api.adjective_request(new_word)
+	for menu_name in menus:
+		menus[menu_name].request_word(new_word)
